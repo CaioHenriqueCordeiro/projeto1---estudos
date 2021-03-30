681aa6ddb9f6b5197a2830b1fa9fcece5a6b5db1
CaioHenriqueCordeiro <chcp.98@gmail.com>
2021-03-30 12:02:39 -0300

    Primeiro commit

---
+++ src/main/java/com/example/projeto1/entity/Role.java
@@ -0,0 +1,30 @@
+package com.example.projeto1.entity;
+
+import javax.persistence.*;
+
+//Classe de Perfis. Para vincular ao usuário
+
+@Entity
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
